f722f28d410181b0fefdeeb1150e6a72682358d9
Manavj99 <manavj99@gmail.com>
2026-01-13 20:37:46 -0500

    New assingment 1 and 2

--- hello_world.py
+++ hello_world.py
@@ -1,23 +1,88 @@
+
 # print hello_world
-print("Hello, World!")
+# print("Hello, World!")
 
-age = 20
-print( "The age is ", age, "years old")
+# age = 20
+# print( "The age is ", age, "years old")
 
-name = "Alice"
-age = 15
-print( "The name is ", name, "and the age is ", age, "years old")
+# name = "Alice"
+# age = 15
+# print( "The name is ", name, "and the age is ", age, "years old")
 
-a = 10
-b= 5
-sum = a + b
-print("SUM: ", sum)
-print("DIFFERENCE: ", a-b)
-print("PRODUCT: ", a*b)
-print("QUOTIENT: ", a/b)
-print("REMAINDER: ", a%b)
-print("POWER: ", a**b)
-print("INTEGER DIVISION: ", a//b)
-print("MODULO: ", a%b)
+# a = 10
+# b= 5
+# sum = a + b
+# print("SUM: ", sum)
+# print("DIFFERENCE: ", a-b)
+# print("PRODUCT: ", a*b)
+# print("QUOTIENT: ", a/b)
+# print("REMAINDER: ", a%b)
+# print("POWER: ", a**b)
+# print("INTEGER DIVISION: ", a//b)
+# print("MODULO: ", a%b)
+# print("COMPARISON: ", a,b)
 
+# x=5
+# y=15
+# if x>0:
+#     print("x is greater than 0")
+# elif x<0:
+#     print("x is less than 0")
+# elif y>0:
+#     print("y is greater than 0")
+# elif y<0:
+#     print("y is less than 0")
+# else:
+#     print("x and y are not greater than 0")
+
+
+# student=["Manav", "John", "Jane", "Jim", "Jill"]
+# for students in student:
+#     if students == "Manav":
+#         print("Manav is a student")
+
+
+# word = "HeLLo WoRlD PyThon"
+# for letter in word:
+#     print(letter.upper())
+
+
+# marks = int(input("Enter your marks: "))
+# print("You got", marks, "marks")
+# if marks >= 90:
+#     print("You got A grade")
+# elif marks >= 80:
+#     print("You got B grade")
+# elif marks >= 70:
+#     print("You got C grade")
+# elif marks >= 60:
+#     print("You got D grade")
+# else:
+#     print("You got F grade")
+
+# secret_number = 7
+# guess = 0
+# while guess != secret_number:
+#     guess = int(input("Enter a number: "))
+#     if guess < secret_number:
+#         print("Too low")
+#     elif guess > secret_number:
+#         print("Too high")
+# print("You guessed the number!")
+
+secret_number = 7
+guess = 0
+guess_count = 3
+while guess != secret_number and guess_count > 0:
+    guess = int(input("Enter a number: "))
+    if guess < secret_number:
+        print("Too low")
+    elif guess > secret_number:
+        print("Too high")
+    guess_count -= 1
+
+if guess_count == 0:
+    print("You failed to guess the number!")
+else:
+    print("You guessed the number!")
 
